358edb0ec8e681cf4aea3eb310e820153aae0b52
Omar Rivera <omarert@gmail.com>
2023-12-11 09:01:19 -0400

    Advanced 2

---
+++ src/advanced/Anotations.java
@@ -0,0 +1,92 @@
+package advanced;
+
+/**
+Java annotation can be used to define the metadata of a Java class or class element. We can use Java annotation at the compile time to instruct the compiler about the build process. Annotation is also used at runtime to get insight into the properties of class elements.
+
+Java annotation can be added to an element in the following way:
+
+@Entity
+Class DemoClass{
+
+}
+We can also set a value to the annotation member. For example:
+
+@Entity(EntityName="DemoClass")
+Class DemoClass{
+
+}
+In Java, there are several built-in annotations. You can also define your own annotations in the following way:
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+   String userRole() default "GUEST";
+}
+Here, we define an annotation FamilyBudget, where userRole is the only member in that custom annotation. The userRole takes only String type values, and the default is "GUEST". If we do not define the value for this annotation member, then it takes the default. By using @Target, we can specify where our annotation can be used. For example, the FamilyBudget annotation can only be used with the method in a class. @Retention defines whether the annotation is available at runtime. To learn more about Java annotation, you can read the tutorial and oracle docs.
+ */
+
+
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+	String userRole() default "GUEST";
+	int budgetLimit() default 0;
+}
+
+class FamilyMember {
+	@FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
+	public void seniorMember(int budget, int moneySpend) {
+		System.out.println("Senior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+
+	@FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
+	public void juniorUser(int budget, int moneySpend) {
+		System.out.println("Junior Member");
+		System.out.println("Spend: " + moneySpend);
+		System.out.println("Budget Left: " + (budget - moneySpend));
+	}
+}
+
+public class Anotations {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String role = in.next();
+			int spend = in.nextInt();
+			try {
+				Class annotatedClass = FamilyMember.class;
+				Method[] methods = annotatedClass.getMethods();
+				for (Method method : methods) {
+					if (method.isAnnotationPresent(FamilyBudget.class)) {
+						FamilyBudget family = method
+								.getAnnotation(FamilyBudget.class);
+						String userRole = family.userRole();
+						int budgetLimit = family.budgetLimit();
+						if (userRole.equals(role)) {
+							if(budgetLimit >= spend){
+								method.invoke(FamilyMember.class.newInstance(),
+										budgetLimit, spend);
+							}else{
+								System.out.println("Budget Limit Over");
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			testCases--;
+		}
+	}
+}
+
+
+
---
+++ src/advanced/Sha_256.java
@@ -0,0 +1,48 @@
+package advanced;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/*
+ Cryptographic hash functions are mathematical operations run on digital data; by comparing the computed hash (i.e., the output produced by executing a hashing algorithm) to a known and expected hash value, a person can determine the data's integrity. For example, computing the hash of a downloaded file and comparing the result to a previously published hash result can show whether the download has been modified or tampered with. In addition, cryptographic hash functions are extremely collision-resistant; in other words, it should be extremely difficult to produce the same hash output from two different input values using a cryptographic hash function.
+
+Secure Hash Algorithm 2 (SHA-2) is a set of cryptographic hash functions designed by the National Security Agency (NSA). It consists of six identical hashing algorithms (i.e., SHA-256, SHA-512, SHA-224, SHA-384, SHA-512/224, SHA-512/256) with a variable digest size. SHA-256 is a 256-bit (32 byte) hashing algorithm which can calculate a hash code for an input of up to 264-1 bits. It undergoes 64 rounds of hashing and calculates a hash code that is a 64-digit hexadecimal number.
+
+Given a string, s, print its SHA-256 hash value.
+ */
+
+public class Sha_256 {
+
+    private static String getSha256(String s) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(s.getBytes(StandardCharsets.UTF_8));
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        System.out.println(getSha256(s));
+        bufferedReader.close();
+    }
+}
